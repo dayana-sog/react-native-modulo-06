0dddebd13a64368e5f17ba43a7f4655555498911
dayana-sog <dayanasog23@gmail.com>
2020-03-10 16:59:11 +0000

    Estilização inicial de Main-page

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
-import {View} from 'react-native';
 
-// import { Container } from './styles';
+import Icons from 'react-native-vector-icons/MaterialIcons';
+
+import {Container, Form, Input, SubmitButton} from './styles';
 
 export default function Main() {
-  return <View />;
+  return (
+    <>
+      <Container>
+        <Form>
+          <Input
+            autoCorrect={false}
+            autoCaptalize="none"
+            placeholder="Adicionar usuários"
+          />
+          <SubmitButton>
+            <Icons name="add" size={20} color="#FFF" />
+          </SubmitButton>
+        </Form>
+      </Container>
+    </>
+  );
 }
 
 Main.navigationOptions = {
-  title: 'Olá Mundo',
+  title: 'Usuários',
 };
